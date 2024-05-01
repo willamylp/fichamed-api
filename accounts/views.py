57689233a2916bc9610fa1d288ca6decec1e9e9d
Willamy Domingos <willamy.wlp@gmail.com>
2024-05-01 20:43:36 -0300

    Fix: Problem with saving password outside of hash format resolved

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth.hashers import make_password
 from app.permissions import GlobalDefaultPermission
 from .models import User
 from .serializers import UserSerializer
@@ -22,6 +23,13 @@ class UserListCreateView(generics.ListCreateAPIView):
         "created_at", "updated_at"
     ]
     ordering = ["id"]
+
+    def perform_create(self, serializer):
+        serializer.save(
+            password=make_password(
+                serializer.validated_data['password']
+            )
+        )
 
 
 class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
