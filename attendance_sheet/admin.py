90a67d446e85ca10485c6a8d4d7e14c5066edc71
Willamy Domingos <willamy.wlp@gmail.com>
2024-05-01 20:51:28 -0300

    Feat: adjust in update AttendanceSheet for Django Admin

--- attendance_sheet/admin.py
+++ attendance_sheet/admin.py
@@ -18,7 +18,6 @@ class AttendanceSheetAdmin(admin.ModelAdmin):
             "fields": (
                 "card_number",
                 "patient_name",
-                "filled_by",
                 "classification",
                 "clinical_history"
             )
@@ -26,3 +25,7 @@ class AttendanceSheetAdmin(admin.ModelAdmin):
     ]
 
     readonly_fields = ["created_at", "updated_at"]
+
+    def save_model(self, request, obj, form, change):
+        obj.filled_by = request.user
+        super().save_model(request, obj, form, change)
