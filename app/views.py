23e28c40330b7499b847e71cf1a9824a1bb8a789
Willamy Domingos <willamy.wlp@gmail.com>
2024-05-05 16:31:25 -0300

    Feat: Adjusts in ApiRoot

--- app/views.py
+++ app/views.py
@@ -14,19 +14,34 @@ class APIRootView(APIView):
                 'token_obtain_pair': reverse('token_obtain_pair', request=request, format=format),
                 'token_refresh': reverse('token_refresh', request=request, format=format),
                 'token_verify': reverse('token_verify', request=request, format=format),
-                'logout': reverse('logout', request=request, format=format),
+                'token_blacklist': reverse('token_blacklist', request=request, format=format),
             },
             'Accounts': {
                 'users_list_create': reverse('users_list_create', request=request, format=format),
-                'user_datail_view/<int:pk>': reverse('user_datail_view', kwargs={'pk': 1}, request=request, format=format),
+                'user_datail_view/<int:pk>': reverse(
+                    'user_datail_view',
+                    kwargs={'pk': 1},
+                    request=request,
+                    format=format
+                ),
             },
             'AttendanceSheet': {
                 'attendance_sheet_list_create': reverse('attendance_sheet_list_create', request=request, format=format),
-                'attendance_sheet_datail_view/<int:pk>': reverse('attendance_sheet_datail_view', kwargs={'pk': 1}, request=request, format=format),
+                'attendance_sheet_datail_view/<int:pk>': reverse(
+                    'attendance_sheet_datail_view',
+                    kwargs={'pk': 1},
+                    request=request,
+                    format=format
+                ),
                 'attendance_sheet_panel_list': reverse('attendance_sheet_panel_list', request=request, format=format),
             },
             'MedicalCareHistory': {
                 'medical_history_list_create': reverse('medical_history_list_create', request=request, format=format),
-                'medical_history_datail_view/<int:pk>': reverse('medical_history_datail_view', kwargs={'pk': 1}, request=request, format=format),
+                'medical_history_datail_view/<int:pk>': reverse(
+                    'medical_history_datail_view',
+                    kwargs={'pk': 1},
+                    request=request,
+                    format=format
+                ),
             },
         })
